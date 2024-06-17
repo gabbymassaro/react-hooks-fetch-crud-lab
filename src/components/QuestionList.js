916e392e3cd01cca87a67f6ac3809129bff82905
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-17 13:39:24 -0400

    Something's broken, I can't get my fetch to populate

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+import QuestionItem from './QuestionItem'
 
-function QuestionList() {
+function QuestionList( ) {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then(response => response.json())
+      .then(data => setQuestions(data))
+  }, [])
+
+  console.log(questions)
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+      {questions.map((item) => (
+          <QuestionItem
+            key={item.id}
+            prompt={item.prompt}
+            answers={item.answers}
+            correctIndex={item.correctIndex}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
